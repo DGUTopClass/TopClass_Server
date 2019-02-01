dd2eda35a88341e9e8fd3205b055f14a62fe17ce
choigone <gowoonvv@gmail.com>
2019-02-01 14:59:29 +0900

    git test

--- src/main/java/com/topclass/workbox/WorkboxApplication.java
+++ src/main/java/com/topclass/workbox/WorkboxApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkboxApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WorkboxApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WorkboxApplication.class, args);
+	}
 
 }
 
--- src/test/java/com/topclass/workbox/WorkboxApplicationTests.java
+++ src/test/java/com/topclass/workbox/WorkboxApplicationTests.java
@@ -9,9 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WorkboxApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
 
