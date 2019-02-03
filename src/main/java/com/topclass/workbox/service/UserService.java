4ecdb32c66a8cd43e4333d365db7278babea6663
cchloe2311 <cchloe2311@daum.net>
2019-02-03 22:56:06 +0900

    2019/02/03 implemented createUser function in Userservice

--- src/main/java/com/topclass/workbox/service/UserService.java
+++ src/main/java/com/topclass/workbox/service/UserService.java
@@ -1,9 +1,15 @@
 package com.topclass.workbox.service;
 
+import com.topclass.workbox.dto.User;
 import com.topclass.workbox.mapper.UserMapper;
 import com.topclass.workbox.model.DefaultRes;
+import com.topclass.workbox.utils.ResponseMessage;
+import com.topclass.workbox.utils.StatusCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Status;
+import javax.transaction.Transactional;
 
 @Slf4j
 @Service
@@ -14,5 +20,9 @@ public class UserService {
         this.userMapper = userMapper;
     }
 
-
+    @Transactional
+    public DefaultRes createUser(final User user){
+        userMapper.insertUser(user);
+        return DefaultRes.res(StatusCode.CREATED, ResponseMessage.CREATED_USER);
+    }
 }
