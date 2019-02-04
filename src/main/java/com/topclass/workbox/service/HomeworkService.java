ea7b442efda0f699976aeca6c1e57ec1b77ba830
cchloe2311 <cchloe2311@daum.net>
2019-02-04 23:59:58 +0900

    2019/02/04 added selectHomeworkPersonal function in HomeworkMapper

--- src/main/java/com/topclass/workbox/service/HomeworkService.java
+++ src/main/java/com/topclass/workbox/service/HomeworkService.java
@@ -20,7 +20,25 @@ public class HomeworkService {
         this.courseMapper = courseMapper;
     }
 
-    // 유저별 과제리스트 get //
+    // 제출예정 과제 목록 받아오기
+    public DefaultRes getToBeSummitedHomeworkList(String userId){
+        List<String> courseIdList = courseMapper.getCourseId(userId);
+
+        List<Homework> homeworkList = new ArrayList<Homework>();
+        for(String courseId : courseIdList){
+            homeworkList.addAll(homeworkMapper.selectHomework(courseId));
+        }
+
+        // 3. 사용자의 탭에 따라 isHanded가 true 혹은 false 인것만 선별
+
+        // 3-2. if isHanded == true >> HW result도 받아옴 >> 데이터를 추가적으로 List에 반영해줌
+        // 따라서, 지금은 howework로 해뒀는데 새로운 데이터 타입(hw을 상속 받은)이 필요
+        // 4. DefaultRes를 하나 만들어서 이를 리턴
+        // ex) DefaultRes.res(StatusCode.OK, ResponseMessage.READ_SUCCESS, calendarTabReqList);
+        return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_HOMEWORK, homeworkList);
+    }
+
+    // 제출기간 만료 과제 목록 받아오기
     public DefaultRes getSummitedHomeworkList(String userId){
         // 왜 int로 userIdx를 받아올깡 ? ~ ?
 
@@ -29,33 +47,13 @@ public class HomeworkService {
         // 2. 각 과목 id를 돌며 간단한 포맷의 hw 데이터를 받아옴
         List<Homework> homeworkList = new ArrayList<Homework>();
         for(String courseId : courseIdList){
-            homeworkList.addAll(homeworkMapper.getSimpleFormatHomework(courseId));
+            homeworkList.addAll(homeworkMapper.selectHomework(courseId));
         }
 
         // 3. 사용자의 탭에 따라 isHanded가 true 혹은 false 인것만 선별
 
         // 3-2. if isHanded == true >> HW result도 받아옴 >> 데이터를 추가적으로 List에 반영해줌
                                                         // 따라서, 지금은 howework로 해뒀는데 새로운 데이터 타입(hw을 상속 받은)이 필요
-        // 4. DefaultRes를 하나 만들어서 이를 리턴
-        // ex) DefaultRes.res(StatusCode.OK, ResponseMessage.READ_SUCCESS, calendarTabReqList);
-        return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_HOMEWORK, homeworkList);
-    }
-
-    public DefaultRes getToBeSummitedHomeworkList(String userId){
-        // 왜 int로 userIdx를 받아올깡 ? ~ ?
-
-        // 1. user가 수강중인 모든 과목의 id를 받아옴
-        List<String> courseIdList = courseMapper.getCourseId(userId);
-        // 2. 각 과목 id를 돌며 간단한 포맷의 hw 데이터를 받아옴
-        List<Homework> homeworkList = new ArrayList<Homework>();
-        for(String courseId : courseIdList){
-            homeworkList.addAll(homeworkMapper.getSimpleFormatHomework(courseId));
-        }
-
-        // 3. 사용자의 탭에 따라 isHanded가 true 혹은 false 인것만 선별
-
-        // 3-2. if isHanded == true >> HW result도 받아옴 >> 데이터를 추가적으로 List에 반영해줌
-        // 따라서, 지금은 howework로 해뒀는데 새로운 데이터 타입(hw을 상속 받은)이 필요
         // 4. DefaultRes를 하나 만들어서 이를 리턴
         // ex) DefaultRes.res(StatusCode.OK, ResponseMessage.READ_SUCCESS, calendarTabReqList);
         return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_HOMEWORK, homeworkList);
