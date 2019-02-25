53362b16f5e382785d1503cc912a16d44ae2a1ca
choigone <gowoonvv@gmail.com>
2019-02-25 18:11:33 +0900

    api4 and api6 about notice done

--- src/main/java/com/topclass/workbox/api/NoticeController.java
+++ src/main/java/com/topclass/workbox/api/NoticeController.java
@@ -3,11 +3,13 @@ package com.topclass.workbox.api;
 
 import com.topclass.workbox.service.NoticeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -20,7 +22,27 @@ public class NoticeController {
         this.noticeService = noticeService;
     }
 
+    // Api 6 : 공지 리스트 불러오기
     @GetMapping("")
-    public ResponseEntity getNotice(@RequestParam("id"))
-        //뭘 보내줘야 그 사람이 듣는 모든 과목의 공지사항을 띄울 수 있을까?
+    public ResponseEntity getNotice(@RequestParam("user_id") final String user_id){
+//        try{
+//
+//        }
+//        catch (Exception e){
+//
+//        }
+        return new ResponseEntity<>(noticeService.getAllNotice(user_id), HttpStatus.OK);
+    }
+
+    // Api 4 : 미확인 공지 개수 불러오기
+    @GetMapping("")
+    public ResponseEntity getUncheckedNoticeCount(@RequestParam("user_id") final String user_id){
+//        try {
+//
+//        }
+//        catch (Exception e){
+//
+//        }
+        return new ResponseEntity<>(noticeService.getUncheckedNoticeCount(user_id), HttpStatus.OK);
+    }
 }
